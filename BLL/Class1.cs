ab4180a48aa71ea969ef6800bea426fa35ab0b97
adnan <aadnanshahh2001@gmail.com>
2023-06-07 00:52:37 +0500

    full proj cloning

---
+++ BLL/Class1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BLL
+{
+    public class Class1
+    {
+        Class2 obj = new Class2();
+
+        
+
+        public bool CreateUser(string username, string password, string email)
+        {
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+                new SqlParameter("@un", username),
+                new SqlParameter("@ps", password),
+                new SqlParameter("@em", email)
+            };
+
+            try
+            {
+                obj.ExecuteNonQuery("spCreateUser", parameters);
+                return true;
+            }
+            catch (SqlException ex)
+            {
+                if (ex.Number == 50000)
+                {
+                    throw new Exception(ex.Message);
+                }
+                else
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        public bool AuthenticateUser(string username, string password)
+        {
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+                new SqlParameter("@un", username),
+                new SqlParameter("@ps", password)
+            };
+
+            DataTable dataTable = obj.ExecuteQuery("spGetUser", parameters);
+
+            return dataTable.Rows.Count > 0;
+        }
+
+        public bool CreateDestination(string destinationName, DateTime departureDate, DateTime returnDate, string transportType, string hotelName, decimal price)
+        {
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+                new SqlParameter("@DestinationName", destinationName),
+                new SqlParameter("@DepartureDate", departureDate),
+                new SqlParameter("@ReturnDate", returnDate),
+                new SqlParameter("@TransportType", transportType),
+                new SqlParameter("@HotelName", hotelName),
+                new SqlParameter("@Price", price)
+            };
+
+            try
+            {
+                obj.ExecuteNonQuery("spCreateDestination", parameters);
+                return true;
+            }
+            catch (SqlException ex)
+            {
+                if (ex.Number == 50000)
+                {
+                    throw new Exception(ex.Message);
+                }
+                else
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        public DataTable GetDestinations()
+        {
+            return obj.ExecuteQuery("spGetDestinations", null);
+        }
+
+    }
+}
---
+++ DAL/Class1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace DAL
+{
+    public class Class2
+    {
+
+               public static string connectionString = "Data Source=DESKTOP-OCJ8U5N\\SQLEXPRESS;Initial Catalog=project;Integrated Security=True";
+             SqlConnection connection=new SqlConnection(connectionString);
+
+            public void Open()
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+            }
+
+            public void Close()
+            {
+                if (connection.State != ConnectionState.Closed)
+                {
+                    connection.Close();
+                }
+            }
+
+            public int ExecuteNonQuery(string storedProcedureName, SqlParameter[] parameters)
+            {
+                SqlCommand command = new SqlCommand(storedProcedureName, connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                if (parameters != null)
+                {
+                    command.Parameters.AddRange(parameters);
+                }
+
+                Open();
+                int result = command.ExecuteNonQuery();
+                Close();
+
+                return result;
+            }
+
+            public DataTable ExecuteQuery(string storedProcedureName, SqlParameter[] parameters)
+            {
+                SqlCommand command = new SqlCommand(storedProcedureName, connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                if (parameters != null)
+                {
+                    command.Parameters.AddRange(parameters);
+                }
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+
+                Open();
+                adapter.Fill(dataTable);
+                Close();
+
+                return dataTable;
+            }
+        }
+
+}
